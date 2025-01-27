d8c6faf18ce6e7365423b92b421e170ce4afa0d6
Abdul Hakeem <cb013168@students.apiit.lk>
2025-01-27 16:25:04 +0530

    footer

--- lib/Screens/AccountScreen.dart
+++ lib/Screens/AccountScreen.dart
@@ -4,6 +4,19 @@ import 'package:the_fution_fork_mad/screens/HomeScreen.dart';
 import 'Menupage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Accpage(),
+    );
+  }
+}
+
 class Accpage extends StatefulWidget {
   const Accpage({super.key});
 
@@ -36,19 +49,19 @@ class _AccpageState extends State<Accpage> {
 
     switch (index) {
       case 0:
-        Navigator.push(
+        Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => const homepage()),
         );
         break;
       case 1:
-        Navigator.push(
+        Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => const MenuPage()),
         );
         break;
       case 2:
-        Navigator.push(
+        Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => const CartPage()),
         );
@@ -63,95 +76,108 @@ class _AccpageState extends State<Accpage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: _isDarkMode ? ThemeData.dark() : ThemeData.light(),
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            elevation: 0,
-            title: const Text(
-              'Account Settings',
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: const Text(
+          'Account Settings',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
+        centerTitle: true,
+        iconTheme: const IconThemeData(color: Colors.black),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.account_circle),
+            title: const Text('Change Username'),
+            onTap: () {
+              // Add functionality for changing the username
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.email),
+            title: const Text('Change Email'),
+            onTap: () {
+              // Add functionality for changing the email
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.lock),
+            title: const Text('Change Password'),
+            onTap: () {
+              // Add functionality for changing the password
+            },
+          ),
+          const Divider(),
+          const Text(
+            'Notification Settings',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications),
+            title: const Text('Enable Notifications'),
+            onTap: () {
+              // Add functionality to enable or disable notifications
+            },
+          ),
+          const Divider(),
+          const Text(
+            'Privacy Settings',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+          ListTile(
+            leading: const Icon(Icons.privacy_tip),
+            title: const Text('Change Privacy Settings'),
+            onTap: () {
+              // Add functionality for privacy settings
+            },
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Browse',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Carts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Account',
+          ),
+        ],
+      ),
+      bottomSheet: Container(
+        height: 50,
+        padding: const EdgeInsets.all(10),
+        child: const Center(
+          child: Text(
+            '© 2025 Abdul Hakeem. All rights reserved.',
+            style: TextStyle(
+              color: Color.fromARGB(255, 0, 0, 0),
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
             ),
-            centerTitle: true,
-            iconTheme: const IconThemeData(color: Colors.black),
           ),
-          body: ListView(
-            padding: const EdgeInsets.all(16),
-            children: <Widget>[
-              ListTile(
-                leading: const Icon(Icons.account_circle),
-                title: const Text('Change Username'),
-                onTap: () {
-                  // Add functionality for changing the username
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.email),
-                title: const Text('Change Email'),
-                onTap: () {
-                  // Add functionality for changing the email
-                },
-              ),
-              ListTile(
-                leading: const Icon(Icons.lock),
-                title: const Text('Change Password'),
-                onTap: () {
-                  // Add functionality for changing the password
-                },
-              ),
-              const Divider(),
-              const Text(
-                'Notification Settings',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              ListTile(
-                leading: const Icon(Icons.notifications),
-                title: const Text('Enable Notifications'),
-                onTap: () {
-                  // Add functionality to enable or disable notifications
-                },
-              ),
-              const Divider(),
-              const Text(
-                'Privacy Settings',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-              ),
-              ListTile(
-                leading: const Icon(Icons.privacy_tip),
-                title: const Text('Change Privacy Settings'),
-                onTap: () {
-                  // Add functionality for privacy settings
-                },
-              ),
-            ],
-          ),
-          bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Browse',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart),
-                label: 'Carts',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Account',
-              ),
-            ],
-          ),
-        ));
+        ),
+      ),
+    );
   }
 }
